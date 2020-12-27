04337f0d745b12d9b2fcef5fbdfb7373fc860473
KNGsadmachine <sansuu716@gmail.com>
2020-12-27 17:15:41 +0900

    Git

--- compro/Kaishin.py
+++ compro/Kaishin.py
@@ -15,36 +15,6 @@
 # result = smtp.sendmail(from_address, to_address, message)
 # print(result)
 
-
-lines = 'WU WD RU WD'
-B = list(map(str, lines.split()))
-print(B)
-Ans = [0,0]
-
-for i in range(len(B)-1):
-    if B[i] != B[i+1]:
-        if B[i] == "WU":
-            Ans[0] += 1
-        elif B[i] == "WD":
-            Ans[0] -= 1
-        elif B[i] == "RU":
-            Ans[1] += 1
-        else:
-            Ans[1] -= 1
-
-    if 2 in Ans:
-        print("Alice")
-        break
-    elif -1 in Ans:
-        print("Alice")
-        break
-    else:
-        for j in range(len(B)-1):
-            if B[j] == B[j+1]:
-                print("Alice")
-                break
-print(Ans)
-
         
 
         
