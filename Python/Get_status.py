ab18c75df56b1b3dbc0ddb341d2e0c1ba6f5aefd
Sadmachine <sansuu716@gmail.com>
2020-12-30 13:37:44 +0900

    TwitterAPIソースの変更

--- Python/Get_status.py
+++ Python/Get_status.py
@@ -1,3 +1,13 @@
 import sys
+import datetime
+import re
 
 print(sys.version)
+
+Start_time = str(datetime.datetime.now())
+Start_time = re.split("[-:. ]", Start_time)
+Start_time.pop(-1)
+Start_time = map(str, Start_time)
+Start_time_str = "".join(Start_time)
+Start_time = int(Start_time_str)
+print(Start_time)
--- Python/getffnum.py
+++ Python/getffnum.py
@@ -2,6 +2,7 @@
 import tweepy
 import datetime
 import csv
+import re
 
 #魔法の言葉
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -24,18 +25,48 @@ for i in range(len(user)-1):
     Follower_count = int(User_info.followers_count)
     Follower_num_list.append(User)
     Follower_num_list.append(Follower_count)
-    
+    Info.append(User)
+    Info.append(User_info)
 
 #終了時間の記述
 End_time = datetime.datetime.now()
 
-Follower_num_list.insert(0,Start_time)
-Follower_num_list.insert(1,End_time)
-
 print(Start_time)
 print(End_time)
 
-with open ("/Users/sadmachine/OneDrive/MASTER/MATLAB/Follow_data/{}.csv".format(End_time), 'w') as f:
+Start_time = str(Start_time)
+Start_time = re.split("[-:. ]", Start_time)
+Start_time.pop(-1)
+Start_time = map(str, Start_time)
+Start_time_str = "".join(Start_time)
+Start_time = int(Start_time_str)
+print(Start_time)
+
+End_time = str(End_time)
+End_time = re.split("[-:. ]", End_time)
+End_time.pop(-1)
+End_time = map(str, End_time)
+End_time_str = "".join(End_time)
+End_time = int(End_time_str)
+print(End_time)
+
+Follower_num_list.insert(0,Start_time)
+Follower_num_list.insert(1,End_time)
+Info.insert(0,Start_time)
+Info.insert(1,End_time)
+
+
+with open ("/Users/kk/OneDrive/MASTER/MATLAB/Follow_data/{}.csv".format(End_time), 'w') as f:
     writer = csv.writer(f)
     writer.writerow (Follower_num_list)
+with open ("/Users/kk/OneDrive/MASTER/MATLAB/Follow_data/archiveFollowNum.csv", 'a') as f:
+    writer = csv.writer(f)
+    writer.writerow (Follower_num_list)
+with open ("/Users/kk/OneDrive/MASTER/MATLAB/info/{}.csv".format(End_time), 'w',encoding='utf_8_sig') as f:
+    writer = csv.writer(f)
+    writer.writerow (Info)
+with open ("/Users/kk/OneDrive/MASTER/MATLAB/info/archiveInfo.csv", 'a',encoding='utf_8_sig') as f:
+    writer = csv.writer(f)
+    writer.writerow (Info)
+
 print("Finish")
