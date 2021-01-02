466ff66c9636a1d828c8ab7c97fb868bfa813ca6
Sadmachine <sansuu716@gmail.com>
2021-01-02 17:06:47 +0900

    Make APP

---
+++ App/Map/Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by K K on 2021/01/02.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
